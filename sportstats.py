f1756f306751ddaabafabc8d93c79898a892cf60
Tommy Prevost <git@tomix.ca>
2025-04-08 17:04:32 -0400

    modification du .dat avec nouvelles valeurs changement de la valeur du départ du .py

--- sportstats.py
+++ sportstats.py
@@ -3,11 +3,17 @@ import requests
 def grab(id):
   reponse = requests.get(f"https://sportstats.one/fr/results/athlete/{id}")
   contenu = str(reponse.content)
-  debut = contenu.find('"pdn":"') + len('"pdn":"')
-  fin = contenu.find('"', debut)
-  return (contenu[debut:fin])
+  debut = contenu.find('"pdn":"')
+  if debut != -1: 
+    debut += len('"pdn":"')
+    fin = contenu.find('"', debut)
+    return (contenu[debut:fin])
+  else:
+    return ""
 
 if __name__ == "__main__":
-  for i in range(1, 760000):
+  for i in range(57040, 760000):
     with open("sportstats.dat", "a") as f:
-      f.write(f"{i}\t{grab(i)}\n")
+      resultat = grab(i)
+      if resultat:
+        f.write(f"{i}\t{resultat}\n")
